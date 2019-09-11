967763a32e1dd206d698741ddb9bb0e2a0c77175
Justin Hunt <Justin.hunt@utoronto.ca>
2019-09-11 17:28:35 -0400

    Add project files.

---
+++ Video Game Store COMP2084-F19/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Video_Game_Store_COMP2084_F19.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
